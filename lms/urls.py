f0041d0e315a2f72c81f84fe62faa53667a4f6d5
Степан Прокопьев <dr.aibolitarise@mail.ru>
2026-04-05 06:17:13 +0900

    feat: маршруты для уроков (Generic) и курсов (через роутер)

---
+++ lms/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import LessonListCreateAPIView, LessonRetrieveUpdateDestroyAPIView
+
+urlpatterns = [
+    # Маршруты для уроков (Generic-классы)
+    path('lessons/', LessonListCreateAPIView.as_view(), name='lesson-list-create'),
+    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyAPIView.as_view(), name='lesson-detail'),
+]
