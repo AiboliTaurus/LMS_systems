5da9098e10cfb6f2e7680416031dca7a749fe906
Степан Прокопьев <dr.aibolitarise@mail.ru>
2026-04-16 04:07:39 +0900

    feat: добавлен PaymentViewSet с фильтрацией и сортировкой

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
-from .models import User
-from .serializers import UserSerializer
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import User, Payment
+from .serializers import UserSerializer, PaymentSerializer, UserWithPaymentsSerializer
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
@@ -11,6 +13,27 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     def get_queryset(self):
-        # Для безопасности: пользователь может видеть только свой профиль
-        # но по заданию пока без авторизации, так что оставляем все
         return User.objects.all()
+
+    def get_serializer_class(self):
+        # для детального просмотра используем расширенный сериализатор
+        if self.action == 'retrieve':
+            return UserWithPaymentsSerializer
+        return UserSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для CRUD операций с платежами
+    Задание 4: Добавлена фильтрация и сортировка
+    """
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['payment_date', 'amount']
+    ordering = ['-payment_date']  # Сортировка по умолчанию: новые сверху
+    filterset_fields = {
+        'course': ['exact', 'isnull'],
+        'lesson': ['exact', 'isnull'],
+        'payment_method': ['exact'],
+    }
