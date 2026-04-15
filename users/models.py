f083909a3de385c8bc3e22f79016b27a5b3f7098
Степан Прокопьев <dr.aibolitarise@mail.ru>
2026-04-16 04:06:23 +0900

    feat: добавлена модель Payment (платежи пользователей)

--- users/models.py
+++ users/models.py
@@ -21,3 +21,69 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    """
+    Модель платежа
+    """
+
+    class PaymentMethod(models.TextChoices):
+        CASH = 'cash', 'Наличные'
+        TRANSFER = 'transfer', 'Перевод на счет'
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='payments',
+        verbose_name='Пользователь'
+    )
+    payment_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата оплаты'
+    )
+    course = models.ForeignKey(
+        'lms.Course',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='payments',
+        verbose_name='Оплаченный курс'
+    )
+    lesson = models.ForeignKey(
+        'lms.Lesson',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='payments',
+        verbose_name='Оплаченный урок'
+    )
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Сумма оплаты'
+    )
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PaymentMethod.choices,
+        default=PaymentMethod.TRANSFER,
+        verbose_name='Способ оплаты'
+    )
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ['-payment_date']
+
+    def __str__(self):
+        if self.course:
+            target_type = 'курс'
+            target_name = self.course.title
+        elif self.lesson:
+            target_type = 'урок'
+            target_name = self.lesson.title
+        else:
+            target_type = 'неизвестно'
+            target_name = 'не указан'
+
+        return f"Платеж от {self.user.email} на сумму {self.amount} ({target_type}: {target_name})"
