08555ff390e38e2cd23068a3bd4acbb5b99ee41d
Степан Прокопьев <dr.aibolitarise@mail.ru>
2026-04-22 05:13:53 +0900

    feat: добавлены эндпоинты /api/token/ и /api/token/refresh/

--- config/urls.py
+++ config/urls.py
@@ -3,18 +3,26 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from lms.views import CourseViewSet
-from users.views import UserProfileViewSet, PaymentViewSet
+from users.views import UserProfileViewSet, PaymentViewSet, UserRegistrationViewSet
 
 # Создаем роутер для ViewSet'ов
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
 router.register(r'users', UserProfileViewSet)
-router.register(r'payments', PaymentViewSet)  # Добавляем маршруты для платежей
+router.register(r'payments', PaymentViewSet)
+router.register(r'register', UserRegistrationViewSet, basename='register')  # 👈 Регистрация
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # JWT эндпоинты (доступны без авторизации)
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # API эндпоинты
     path('api/', include(router.urls)),
     path('api/', include('lms.urls')),
 ]
