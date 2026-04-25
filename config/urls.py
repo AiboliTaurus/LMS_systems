19c3f0a0e1b82107ce6f3cdf6dc363a7db9ed1ad
Степан Прокопьев <dr.aibolitarise@mail.ru>
2026-04-26 02:24:56 +0900

    feat: добавлен маршрут /api/subscribe/ для подписки

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from lms.views import CourseViewSet
+from lms.views import CourseViewSet, SubscriptionAPIView
 from users.views import UserProfileViewSet, PaymentViewSet, UserRegistrationViewSet
 
 # Создаем роутер для ViewSet'ов
@@ -13,21 +13,23 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
 router.register(r'users', UserProfileViewSet)
 router.register(r'payments', PaymentViewSet)
-router.register(r'register', UserRegistrationViewSet, basename='register')  # 👈 Регистрация
+router.register(r'register', UserRegistrationViewSet, basename='register')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # JWT эндпоинты (доступны без авторизации)
+    # JWT эндпоинты
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # API эндпоинты
     path('api/', include(router.urls)),
     path('api/', include('lms.urls')),
+
+    # Подписка
+    path('api/subscribe/', SubscriptionAPIView.as_view(), name='subscription'),
 ]
 
-# Добавляем URL для медиа-файлов в режиме разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
